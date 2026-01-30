26f35a1ffb309c60ea87cdd419e5b0589effae25
rakan-osama <rakanosama7@gmail.com>
2026-01-30 10:41:02 +0200

    Add htaccess file for public folder redirection and update dashboard route to /admin

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,10 @@ Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('l
 Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('login.store');
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 
+
+
 Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard', [PageController::class, 'index'])->name('dashboard');
+    Route::get('/admin', [PageController::class, 'index'])->name('dashboard');
 });
 
 // Admin Routes
